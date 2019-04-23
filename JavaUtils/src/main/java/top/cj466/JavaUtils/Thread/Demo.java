a0cc56c88c0f4bcd481df0f56c4a5ef34c4ef690
e <cj466@foxmail.com>
2019-04-23 17:14:03 +0800

    Demo01

---
+++ JavaUtils/src/main/java/top/cj466/JavaUtils/Thread/Demo.java
@@ -0,0 +1,12 @@
+package top.cj466.JavaUtils.Thread;
+
+/**
+ * @ClassName Demo
+ * @Description TODO
+ * @Author w
+ * @Date 2019/4/23 17:11
+ * @Version 1.0
+ * @Since JDK 1.8
+ */
+public class Demo {
+}
